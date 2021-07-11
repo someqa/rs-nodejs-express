50f931e1c154149c86edd0ba41f718a8b70f5206
unknown <olga.qa@yahoo.com>
2021-07-11 21:28:29 +0300

    fix: remove unnecessary static

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  static getHello(): string {
+  // eslint-disable-next-line class-methods-use-this
+  getHello(): string {
     return 'Hello World!';
   }
 }
